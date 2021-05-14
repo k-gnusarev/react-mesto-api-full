7e7702e9e12c83c0081a89b506b21ed8c0daacd2
k-gnusarev <kgnusaryov@yandex.ru>
2021-05-14 10:31:04 +0400

    Ðfix auth stuff

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,9 +6,12 @@ const AuthError = require('../errors/AuthError');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(req.headers);
+  console.log(authorization);
+  console.log({authorization});
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация да что ж тут происходит тооооо');
+    throw new AuthError('Необходима авторизация ошибкааааааааа');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -17,7 +20,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'}`);
   } catch (err) {
-    throw new AuthError('Необходима авторизация где ошибка');
+    throw new AuthError('Необходима авторизация');
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
