518f4ba1b53e3647658286cd54679d12e22b65b6
k-gnusarev <kgnusaryov@yandex.ru>
2021-05-16 18:25:49 +0400

    Ðfix auth error

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
-//const AuthError = require('../errors/AuthError');
+const AuthError = require('../errors/AuthError');
 const NotFoundError = require('../errors/NotFoundError');
 
 // eslint-disable-next-line consistent-return
@@ -14,11 +14,19 @@ module.exports = (req, res, next) => {
 
   const token = authorization.replace('Bearer ', '');
   let payload;
-
+  console.log('1. let payload');
+  console.log(payload);
   try {
+    console.log('2. process.env');
+    console.log(process.env);
+    console.log('NODE_ENV, JWT_SECRET');
+    console.log(NODE_ENV);
+    console.log(JWT_SECRET);
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'}`);
   } catch (err) {
-    throw new NotFoundError('Необходима авторизация здесь');
+    console.log('3. error');
+    console.log(err);
+    throw new AuthError('Необходима авторизация');
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
